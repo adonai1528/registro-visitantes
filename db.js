d42a3f9c4882d9483712459df1e9d8802e280048
adonai1528 <adonaisilvadosreis67@gmail.com>
2025-07-04 06:24:25 -0300

    exibir variáveis do process.env

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mysql = require('mysql');
 
+console.log('🔍 process.env.DB_HOST:', process.env.DB_HOST);
+console.log('🔍 process.env.DB_USER:', process.env.DB_USER);
+console.log('🔍 process.env.DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('🔍 process.env.DB_NAME:', process.env.DB_NAME);
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -7,15 +12,9 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-console.log("🔧 Configuração do banco:", {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME
-});
-
 connection.connect((err) => {
     if (err) {
-        console.error('Erro ao conectar no MySQL:', err.message);
+        console.error('❌ Erro ao conectar no MySQL:', err.message);
         return;
     }
     console.log('🟢 Conectado ao banco de dados MySQL!');
